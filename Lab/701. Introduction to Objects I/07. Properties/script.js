5419e041129ed49dabc3006aedd42b632f46bca0
Mauricio S. Perez <maurime2@gmail.com>
2015-11-29 22:14:51 -0800

    Signed-off-by: Mauricio S. Perez <maurime2@gmail.com>

---
+++ Lab/701. Introduction to Objects I/07. Properties/script.js
@@ -0,0 +1,10 @@
+var Spencer = {
+  age: 22,
+  country: "United States"
+};
+
+// make your own object here called me
+var me = {
+  age: 29,
+  country: "United States"
+};
---
+++ Lab/701. Introduction to Objects I/13. Function Review/script.js
@@ -0,0 +1,10 @@
+// Accepts a number x as input and returns its square
+var multiply = function (x,y) {
+  return x * y;
+};
+
+// Write the function multiply below
+// It should take two parameters and return the product
+
+
+console.log(multiply(4,5));

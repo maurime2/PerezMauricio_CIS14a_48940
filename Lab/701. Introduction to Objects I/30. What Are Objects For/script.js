15c903d92bcebc5aedb74b56dc1f147e3c589772
Mauricio S. Perez <maurime2@gmail.com>
2015-11-30 00:01:10 -0800

    Signed-off-by: Mauricio S. Perez <maurime2@gmail.com>

---
+++ Lab/701. Introduction to Objects I/30. What Are Objects For/script.js
@@ -0,0 +1,9 @@
+var spencer = {
+  age: 22,
+  country: "United States"
+};
+
+// make spencer2 here with constructor notation
+var spencer2 = new Object();
+spencer2.age = spencer.age;
+spencer2.country = spencer.country;
